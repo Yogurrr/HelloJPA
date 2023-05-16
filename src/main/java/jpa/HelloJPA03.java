f333524d8aa3bded838ad67b4f6f1084ab040c9b
LeeSeungYeon <masakisqq@naver.com>
2023-05-16 11:16:27 +0900

    jpql 실습 3 - 서브쿼리, 동적쿼리

--- src/main/java/jpa/HelloJPA03.java
+++ src/main/java/jpa/HelloJPA03.java
@@ -6,6 +6,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import javax.persistence.TypedQuery;
+import java.util.ArrayList;
 import java.util.List;
 
 public class HelloJPA03 {
@@ -17,35 +18,35 @@ public class HelloJPA03 {
             // 사원 데이터 조회 - TypedQuery
             // createQuery(질의문, 리턴될객체종류)
             String jpql = "select e from Employee as e";
-//            List<Employees> emps = em.createQuery(jpql, Employees.class).getResultList();
-//            for (Employees emp : emps) System.out.println(emp);
+            List<Employee> emps = em.createQuery(jpql, Employee.class).getResultList();
+//            for (Employee emp : emps) System.out.println(emp);
 
             // 사원 데이터 조회 - Query : 이름, 부서번호, 입사일
             // createQuery(질의문)
-            jpql = "select fname, deptid, hdate from Employee e";
+//            jpql = "select fname, deptid, hdate from Employee e";
 //            List<Object[]> items = em.createQuery(jpql).getResultList();
 //            for (Object[] item : items) System.out.println(item[0] + "/" + item[1] + "/" + item[2]);
 
             // 사원 직책 조회 - jobid가 IT_PROG인 사원
             // 파라메터 바인딩 - :파라메터명, ?순번
-            jpql = "select e from Employee e where jobid = :jobid";   // 이름 기반
-            // jpql = "select e from Employees e where jobid = ?1";   // 위치 기반
-            TypedQuery<Employee> query = em.createQuery(jpql, Employee.class);
-            query.setParameter("jobid", "IT_PROG");
+//            jpql = "select e from Employee e where jobid = :jobid";   // 이름 기반
+            // jpql = "select e from Employee e where jobid = ?1";   // 위치 기반
+//            TypedQuery<Employee> query = em.createQuery(jpql, Employee.class);
+//            query.setParameter("jobid", "IT_PROG");
             // query.setParameter(1, "IT_PROG");
 //            emps = query.getResultList();
 
-//            for(Employees emp : emps) System.out.println(emp);
+            for(Employee emp : emps) System.out.println(emp);
 
             // 사원 평균 연봉 조회 - TypedQuery
-            jpql = "select avg(sal) from Employee e";
+//            jpql = "select avg(sal) from Employee e";
 //            Double avgsal = em.createQuery(jpql, Double.class).getSingleResult();
 
 //            System.out.println(avgsal);
 
             // 사원 직책 수 조회
             // jpql = "select distinct count(jobid) from Employees e";
-            jpql = "select distinct count(distinct jobid) from Employee e";
+//            jpql = "select distinct count(distinct jobid) from Employee e";
 //            Long cntjob = em.createQuery(jpql, Long.class).getSingleResult();
 
 //            System.out.println(cntjob);
@@ -53,22 +54,75 @@ public class HelloJPA03 {
             // jobid로 정렬 후 3페이지 조회 : 페이징 (페이지당 출력건수 : 15)
             // setFirstResult(startpos) : 페이징 시작 위치
             // setMaxResult(getdatacnt) : 조회할 데이터 수
-            jpql = "select e from Employee e order by jobid";
-//            List<Employees> pemps = em.createQuery(jpql, Employees.class).setFirstResult(30).setMaxResults(15).getResultList();
+//            jpql = "select e from Employee e order by jobid";
+//            List<Employee> pemps = em.createQuery(jpql, Employees.class).setFirstResult(30).setMaxResults(15).getResultList();
 
-//            for(Employees pemp : pemps) System.out.println(pemp);
+//            for(Employee pemp : pemps) System.out.println(pemp);
 
             // 직책별 평균 연봉 조회
-            jpql = "select jobid, avg(sal), count(jobid) from Employee e group by jobid";
+//            jpql = "select jobid, avg(sal), count(jobid) from Employee e group by jobid";
 //            List<Object[]> items = em.createQuery(jpql).getResultList();
 
 //            for(Object[] item : items) System.out.println(item[0] + "/" + item[1] + "/" + item[2]);
 
             // 사원이름, 직책, 부서명 조회 : join
-            jpql = "select e.fname, e.jobid, e.deptid, d.dname from Employee e inner join e.department d";
-            List<Object[]> items = em.createQuery(jpql).getResultList();
+//            jpql = "select e.fname, e.jobid, e.deptid, d.dname from Employee e inner join e.department d";
+//            List<Object[]> items = em.createQuery(jpql).getResultList();
 
-            for(Object[] item : items) System.out.println(item[0] + "/" + item[1] + "/" + item[2] + "/" + item[3]);
+//            for(Object[] item : items) System.out.println(item[0] + "/" + item[1] + "/" + item[2] + "/" + item[3]);
+
+
+            // 부서번호가 60번인 사원들의 이름, 직책, 부서명 조회 : join
+//            jpql = " select e.fname, e.jobid, e.deptid from Employee e inner join e.department d where e.deptid = 60 ";
+//            List<Object[]> items = em.createQuery(jpql).getResultList();
+
+//            for(Object[] item : items) System.out.println(item[0] + "/" + item[1] + "/" + item[2]);
+
+
+            // 부서명이 IT인 사원의 사번과 입사일 조회 : 서브쿼리
+//            jpql = " select empid, hdate from Employee e where deptid = (select deptid from Department d where dname = 'IT') ";
+//            List<Object[]> items = em.createQuery(jpql).getResultList();
+
+//            for(Object[] item : items) System.out.println(item[0] + "/" + item[1]);
+
+
+            // 이름, 직책, 연봉으로 사원 조회 : 동적 쿼리
+            String fname = "";
+            String jobid = "";
+            Integer sal = 0;   // null 체크를 위해 클래스형으로 선언
+
+            jpql = " select e from Employee e";
+            List<String> cndtns = new ArrayList<>();   // 조건절 저장 변수
+
+            if (fname != null) {
+                cndtns.add(" fname like concat('%', :fname, '%') ");
+            }
+            if (jobid != null) {
+                cndtns.add(" jobid = :jobid ");
+            }
+            if (sal != null) {
+                cndtns.add(" sal >= :sal ");
+            }
+
+            if (!cndtns.isEmpty()) {   // 조건식이 하나라도 존재한다면
+                jpql += " where " + String.join(" and ", cndtns);
+            }
+
+            TypedQuery<Employee> query = em.createQuery(jpql, Employee.class);
+            if (fname != null) {
+                query.setParameter("fname", fname);
+            }
+            if (jobid != null) {
+                query.setParameter("jobid", jobid);
+            }
+            if (sal != null) {
+                query.setParameter("sal", sal);
+            }
+
+            emps = query.getResultList();
+            for (Employee e : emps) {
+                System.out.println(e);
+            }
 
         } catch (Exception ex) {
             ex.printStackTrace();
